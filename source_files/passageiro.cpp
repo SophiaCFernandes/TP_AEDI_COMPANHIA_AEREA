3c4f6d32bf7fa483c4c389d4fb792dc2316a171d
Kethellen <kethellensilvap14@gmail.com>
2024-12-07 21:24:22 -0300

    Add arquivos da classe passageiro com metodos get e set

---
+++ source_files/passageiro.cpp
@@ -0,0 +1,82 @@
+#include "passageiro.h"
+
+int maiorCodigo = 0;
+
+/**
+ * Construtor padrao da classe Passageiro
+ */
+Passageiro::Passageiro():Pessoa(){
+    endereco = "\0";
+    pontosDeFidelidade = 0;
+}
+
+/**
+ * Metodo que cria a string de dados para armazenamento no arquivo
+ */
+string Passageiro::criaStringDeDados() {
+    return this->codigo + "," + 
+    this->nome + "," + 
+    this->telefone + "," +
+    this->endereco + "," + 
+    to_string(this->pontosDeFidelidade) + 
+    ";\n";
+}
+
+/**
+ * Metodo para definir acessar e definir o atributo endereco da classe pessoa
+ */
+void Passageiro::setEndereco(string endereco){
+    this->endereco = endereco;
+}
+
+/**
+ * Metodo para definir acessar o atributo endereco da classe pessoa
+ */
+string Passageiro::getEndereco(){
+    return endereco;
+}
+
+/**
+ * Metodo para definir acessar e definir o atributo codigo da classe pessoa
+ */
+void Passageiro::setCodigo(){
+    this->codigo = "P" + to_string(maiorCodigo++);
+}
+
+/**
+ * Metodo para definir acessar o atributo codigo da classe pessoa
+ */
+string Passageiro::getCodigo(){
+    return codigo;
+}
+
+/**
+ * Metodo para exibir os atributos da classe pessoa
+ */
+void Passageiro::mostrarPessoa(){
+    cout << "Nome da pessoa: " << getPessoa() << endl;
+    cout << "Telefone: " << getTelefone() << endl;
+    cout << "Endereco: " << getEndereco() << endl;
+    cout << "Codigo: " << getCodigo() << endl;
+}
+
+void adicionaPassageiro(){
+    string nome, telefone, endereco;
+    Passageiro p;
+
+    cout << "Digite o nome do passageiro: ";
+    cin.ignore(); //Limpa o buffer de entrada do primeiro getline
+    getline(cin, nome);
+    p.setPessoa(nome);
+
+    cout << "Digite o telefone do passageiro: ";
+    getline(cin, telefone);
+    p.setTelefone(telefone);
+
+    cout << "Digite o endereco do passageiro: ";
+    getline(cin, endereco);
+    p.setEndereco(endereco);
+    p.setCodigo();
+
+    cout << "Cadastro adicionado com sucesso!" << endl;
+}
---
+++ source_files/passageiro.h
@@ -0,0 +1,29 @@
+#ifndef PASSAGEIRO_H
+#define PASSAGEIRO_H
+
+#include "pessoa.h"
+
+/**
+ * Classe Passageiro que herda de Pessoa
+ */
+class Passageiro : public Pessoa {
+private:
+    string endereco;
+    int pontosDeFidelidade;
+
+public:
+    Passageiro();
+    string criaStringDeDados();
+
+    void setEndereco(string endereco);
+    string getEndereco();
+
+    void setCodigo();
+    string getCodigo();
+
+    void mostrarPessoa();
+};
+
+void adicionaPassageiro();
+
+#endif
